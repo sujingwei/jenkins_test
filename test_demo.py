5d309d63388885842ff116da3fb1635460d7d5ac
苏经伟 <348149047@qq.com>
2020-09-19 13:33:07 +0800

    jenkins读html文件

--- test_demo.py
+++ test_demo.py
@@ -1,7 +1,23 @@
-import time
+import time, os, datetime
 def test():
+    rs = '';
     for i in range(4):
-        print("jenkkins test demo! git testing", i)
+        body = "jenkkins test demo! git testing: " + str(datetime.datetime.now())
+        print(body)
+        rs += "<li>" + body + "</li>"
         time.sleep(1)
+    with open('report/repoart_demo.html', 'w+') as f:
+        temp = '''
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>report</title>
+            </head>
+            <body>
+                <ul>
+        ''' + rs + "</ul></body></html>"
+        f.write(temp)
 if __name__ == '__main__':
 	test()
