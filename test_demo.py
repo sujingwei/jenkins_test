58d8087c26ef8c09d4e07b49cb87f54dfd34f7e4
苏经伟 <348149047@qq.com>
2020-09-19 13:06:36 +0800

    jenkin绑定git,修改代码测试

--- test_demo.py
+++ test_demo.py
@@ -1,7 +1,7 @@
 import time
 def test():
     for i in range(4):
-        print("jenkkins test demo", i)
+        print("jenkkins test demo! git testing", i)
         time.sleep(1)
 if __name__ == '__main__':
 	test()
